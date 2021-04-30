bb5034a2c5d06c7125b17de0fb05abdb6f2e644a
YoYoXC <43702866+YoYoXC@users.noreply.github.com>
2021-04-30 21:43:49 +0300

    Update YoYo.php

--- YoYo.php
+++ YoYo.php
@@ -1,27 +1,17 @@
 <?php
 /*
-** @script		YoYo class for text encryption with password v1.2
-** @author 		Glitch3R(YoYo)
+** @script	YoYo class for text encryption with password v1.2
+** @author	Glitch3R(YoYo)
 ** @copyright	Copyright (C) 2020
 */
 
 class YoYo {
 	
+	public $compress = true; # custom compression 
 	
-	public $compress = false; # compression (BETA) v0.1 
+	public $gzip = true; # compression with gzip
 	
-	
-	public $gzip = false; # compression with gzip
-	
-	
-	public $base64 = false; # encode/decode with base64
-	
-	
-	private	$zero = array('a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z');
-	
-	
-	private	$one = array('A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z');
-	
+	public $base64 = true; # encode/decode with base64
 	
 	private	function inArray($object, $array) {
 		$key = false;
@@ -31,7 +21,6 @@ class YoYo {
 		return $key;
 	}
 	
-	
 	private function TTA($text) {
 		$var = array();
 		for($i=0; strlen($text)>$i; $i++){
@@ -39,7 +28,6 @@ class YoYo {
 		}
 		return implode(' ', $var);
 	}
-	
 	
 	private function ATT($ascii) {
 		$var = array();
@@ -49,7 +37,6 @@ class YoYo {
 		}
 		return implode($var);
 	}
-	
 	
 	private function NumbersToBinary($object) {
 		$ch = array('0', '1', 'z', 'u', '2', '3', '4', '5', '6', '7', '8', '9', ' ');
@@ -63,7 +50,6 @@ class YoYo {
 		return $return;
 	}
 	
-	
 	private function BinaryToNumbers($object) {
 		$ch = array("1111", "0111", "1110", "0011", "0110", "1100", "0001", "0010", "0100", "1000", "0000", "u", "z");
 		$bi = array(' ', "9", "8", "7", "6", "5", "4", "3", "2", "u", "z", "1", "0");
@@ -75,7 +61,6 @@ class YoYo {
 		$return = implode($obj);
 		return $return;
 	}
-	
 	
 	private function YoYoAction($number, $object, $order='ASC') {
 		if($number == '0') {
@@ -213,67 +198,104 @@ class YoYo {
 		
 	}
 	
-	
 	# Compression
-	private function compress($string) {
-		$split_compress = str_split($string, 1);
-		$count = count($split_compress)-1;
-		for($i=0;$i<=$count;$i++) {
-			if($split_compress[$i] == '0') {
-				$array = $this->zero;
-			}
-			else if($split_compress[$i] == '1') {
-				$array = $this->one;
-			}
-			if($i > 0 && $split_compress[$i] == $split_compress[$i-1]) {
-				if($NR == 26) {
-					$NR = 1;
+	private function COMPRESS($zr) {
+		$split = str_split($zr, 1);
+		$count = count($split)-1;
+		
+		$first = null;
+		$nr = 0;
+		$count_chars = 1;
+		$arr = null;
+		
+		foreach($split as $char) {
+			if($nr == 0) { $first = $char; }
+			if($nr > 0) {
+				if($char == $split[$nr-1]) {
+					$count_chars++;
 				}
 				else {
-					$NR ++;
+					$count_chars = 1;
+				}
+			}
+			if($nr < ($count)) {
+				if($char == $split[$nr+1]) {
+					$last = false;
+				}
+				else {
+					$last = true;
 				}
 			}
 			else {
-				$NR = 1;
-			}
-			$last = false;
-			if(!isset($split_compress[$i+1]) || isset($split_compress[$i+1]) && $split_compress[$i+1] != $split_compress[$i] || $NR == 26) {
 				$last = true;
 			}
 			if($last) {
-				$compressed[] = $array[$NR-1];
+				$arr[] = $count_chars;
+			}
+			$nr++;
+		}
+
+		$newarr = [];
+		foreach($arr as $ARR) {
+			if($ARR > 9) {
+				$newarr[] = 9;
+				$newarr[] = 0;
+				$diff = abs(9 - $ARR);
+				for(;;) {
+					if($diff > 9) {
+						$newarr[] = 9;
+						$newarr[] = 0;
+					}
+					else {
+						$newarr[] = $diff;
+					}
+					$diff-=9;
+					if($diff <= 0) {
+						break;
+					}
+				}
+			}
+			else {
+				$newarr[] = $ARR;
 			}
 		}
-		return implode($compressed);
+		
+		return $first.'/'.implode($newarr);
 	}
-
+	
 	
 	# Decompression
-	private function decompress($content) {
-		$split_uncompress = str_split($content, 1);
-		$count = count($split_uncompress)-1;
-		for($i=0;$i<=$count;$i++) {
-			$char = $split_uncompress[$i];
-			if(ctype_lower($char)) {
-				$what = '0';
-				$howMany = (($this->inArray($char, $this->zero))+1);
+	private function DECOMPRESS($compressed) {
+		
+		list($first, $rest) = explode('/', $compressed);
+		
+		$wasFirst = NULL;
+		$return = null;
+		
+		$split = str_split($rest, 1);
+		foreach($split as $number) {
+			if($wasFirst == NULL) {
+				$wasFirst = $first;
 			}
-			else if(ctype_upper($char)){
-				$what = '1';
-				$howMany = (($this->inArray($char, $this->one))+1);
+			else {
+				if($wasFirst == '0') {
+					$wasFirst = '1';
+				}
+				else if($wasFirst == '1') {
+					$wasFirst = '0';
+				}
 			}
-			for($iii=1;$iii<=$howMany;$iii++) {
-				$uncompress[] = $what;
-			}
+			$return[] = str_repeat($wasFirst, $number);
 		}
-		return implode($uncompress);
+		
+		return implode($return);
 	}
 	
 	
 	# encrypt
 	public function encrypt($password,$content) {
 		$content = $this->Algorithm($password, $this->NumbersToBinary($this->TTA($content)));
-		if($this->compress) { $content = $this->compress($content); }
+		if($this->compress) { $content = $this->COMPRESS($content); }
 		if($this->gzip) { $content = gzdeflate($content,9); }
 		if($this->base64) { $content = base64_encode($content); }
 		return $content;
@@ -284,7 +306,7 @@ class YoYo {
 	public function decrypt($password, $content) {
 		if($this->base64) { $content = base64_decode($content); }
 		if($this->gzip) { $content = gzinflate($content); }
-		if($this->compress) { $content = $this->decompress($content); }
+		if($this->compress) { $content = $this->DECOMPRESS($content); }
 		$content = $this->ATT($this->BinaryToNumbers($this->Algorithm($password, $content, 'decode')));
 		return $content;
 	}
